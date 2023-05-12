029f42e884181ce792692d94aee4c25e9d7fedaa
Moritz R. Hüllmann <mhuellmann@googlemail.com>
2023-05-13 00:39:17 +0200

    Restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use std::borrow::BorrowMut;
+use std::fmt;
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum ProcessState {
+    New,
+    Ready,
+    Running,
+    Suspended,
+    Terminated,
+}
 
 #[derive(Debug, Ord, Eq, PartialEq, PartialOrd, Clone, Copy)]
 struct Process {
@@ -8,6 +18,7 @@ struct Process {
     waiting_time: u64,
     priority: u64,
     id: usize,
+    state: ProcessState,
 }
 
 impl Process {
@@ -19,6 +30,7 @@ impl Process {
             priority,
             waiting_time: 0,
             serviced_time: 0,
+            state: ProcessState::New,
         }
     }
 }
@@ -29,7 +41,7 @@ struct Processor {
     current: Option<usize>,
     processes: Vec<Process>,
     strategy: Strategy,
-    time: u64,
+    system_time: u64,
 }
 
 impl Processor {
@@ -38,153 +50,200 @@ impl Processor {
             current: None,
             processes,
             strategy,
-            time: 0,
+            system_time: 0,
         }
     }
 
-    fn tick(&mut self) {
+    fn first_come_first_serve(processes: Vec<Process>) -> Self {
+        Self::new(processes, first_come_first_serve)
+    }
+
+    fn shortest_job_first(processes: Vec<Process>) -> Self {
+        Self::new(processes, shortest_job_first)
+    }
+
+    fn lowest_priority_first(processes: Vec<Process>) -> Self {
+        todo!("lowest_priority_first")
+    }
+
+    fn lowest_priority_first_preemptive(processes: Vec<Process>) -> Self {
+        todo!("lower_priority_first_preemptive")
+    }
+
+    fn shortest_remaining_time(processes: Vec<Process>) -> Self {
+        todo!("shortest_remaining_time")
+    }
+
+    fn round_robin(processes: Vec<Process>) -> Self {
+        todo!("round_robin")
+    }
+
+    fn multi_level_feedback_queue(processes: Vec<Process>) -> Self {
+        todo!("multi_level_feedback_queue")
+    }
+
+    fn tick(&mut self) -> u64 {
+
         (&self.strategy)(self);
-        self.time += 1;
+        self.system_time += 1;
+
+        self.system_time
+    }
+
+    fn run(&mut self) {
+        let mut log: Vec<SchedulingEvent> = Vec::new();
+
+        for _ in 0..30 {
+            let id_prev = self.current_process_id();
+            let system_time = self.tick();
+            let id_curr = self.current_process_id();
+
+            let prev_process = {
+                match id_prev {
+                    Some(id) => Some(self.get_process(id)),
+                    None => None
+                }
+            };
+
+            let curr_process = {
+                match id_curr {
+                    Some(id) => Some(self.get_process(id)),
+                    None => None
+                }
+            };
+
+            let event = SchedulingEvent::new(system_time, prev_process, curr_process);
+            log.push(event);
+        }
+
+        for event in log {
+            println!("{}", event);
+        }
+    }
+
+    fn get_process(&self, id: usize) -> &Process {
+            self.processes.iter().find(|p| p.id == id).unwrap()
+    }
+
+    fn current_process_mut(&mut self) -> Option<&mut Process> {
+        if let Some(id) = self.current {
+            return Some(self.processes.iter_mut().find(|p| p.id == id).unwrap());
+        }
+        None
+    }
+
+    fn current_process_ref(&self) -> Option<&Process> {
+        if let Some(id) = self.current {
+            return Some(self.get_process(id));
+        }
+        None
+    }
+
+    fn current_process_id(&self) -> Option<usize> {
+        if let Some(process) = self.current_process_ref() {
+            return Some(process.id);
+        }
+        None
+    }
+
+    fn set_current_process_state(&mut self, state: ProcessState) {
+        if let Some(process_mut) = self.current_process_mut() {
+            process_mut.state = state;
+        }
     }
 }
 
-#[derive(Debug)]
-enum Event {
-    Started,
-    Running,
-    Finished,
-    Interrupted,
-    Resumed,
-    ProcessorIdle,
-}
-
+#[allow(dead_code)]
 #[derive(Debug)]
 struct SchedulingEvent {
-    time: u64,
-    process: Option<usize>,
-    event: Event,
+    system_time: u64,
+    start_of_tick: Option<(usize, ProcessState)>,
+    end_of_tick: Option<(usize, ProcessState)>
 }
 
 impl SchedulingEvent {
-    fn from(previous_process: Option<usize>, processor: &Processor) -> Vec<SchedulingEvent> {
-        let mut local_log = vec![];
-        match previous_process {
-            None => {
-                if let Some(_) = processor.current {
-                    local_log.push(SchedulingEvent {
-                        time: processor.time,
-                        process: processor.current,
-                        event: Event::Started,
-                    });
-                } else {
-                    local_log.push(SchedulingEvent {
-                        time: processor.time,
-                        process: processor.current,
-                        event: Event::ProcessorIdle,
-                    });
-                }
+    fn new(system_time: u64, start_of_tick: Option<&Process>, end_of_tick: Option<&Process>) -> Self {
+        let start = {
+            match start_of_tick {
+                Some(process) => Some((process.id, process.state)),
+                None => None,
             }
-            Some(prev_id) => match processor.current {
-                None => {
-                    local_log.push(SchedulingEvent {
-                        time: processor.time,
-                        process: Some(prev_id),
-                        event: Event::Finished,
-                    });
-                }
-                Some(cur_id) => {
-                    if prev_id == cur_id {
-                        local_log.push(SchedulingEvent {
-                            time: processor.time,
-                            process: processor.current,
-                            event: Event::Running,
-                        });
-                    } else {
-                        let cur_proc = processor.processes.iter().find(|p| p.id == cur_id).unwrap();
-                        let prev_proc = processor
-                            .processes
-                            .iter()
-                            .find(|p| p.id == prev_id)
-                            .unwrap();
-                        if prev_proc.remaining_time != 0 {
-                            local_log.push(SchedulingEvent {
-                                time: processor.time,
-                                process: Some(prev_id),
-                                event: Event::Interrupted,
-                            });
-                            if cur_proc.serviced_time != 0 {
-                                local_log.push(SchedulingEvent {
-                                    time: processor.time,
-                                    process: Some(cur_id),
-                                    event: Event::Resumed,
-                                });
-                            } else {
-                                local_log.push(SchedulingEvent {
-                                    time: processor.time,
-                                    process: Some(cur_id),
-                                    event: Event::Started,
-                                });
-                            }
-                        } else {
-                            local_log.push(SchedulingEvent {
-                                time: processor.time,
-                                process: Some(prev_id),
-                                event: Event::Finished,
-                            });
-                            local_log.push(SchedulingEvent {
-                                time: processor.time,
-                                process: Some(cur_id),
-                                event: Event::Started,
-                            });
-                        }
-                    }
-                }
-            },
+        };
+        let end = {
+            match end_of_tick {
+                Some(process) => Some((process.id, process.state)),
+                None => None,
+            }
+        };
+        Self {
+            system_time,
+            start_of_tick: start,
+            end_of_tick: end,
         }
-        local_log
+    }
+}
+
+impl fmt::Display for SchedulingEvent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:05}: ", self.system_time)?;
+        match self.start_of_tick {
+            Some((id, state)) => write!(f, "{state:?}({id})")?,
+            None => write!(f, "None")?,
+        };
+        write!(f, " -> ")?;
+        match self.end_of_tick{
+            Some((id, state)) => write!(f, "{state:?}({id})")?,
+            None => write!(f, "None")?,
+        };
+        Ok(())
     }
 }
 
 fn first_come_first_serve(processor: &mut Processor) {
-    if let Some(id) = processor.current {
-        let mut current = processor.processes.iter_mut().find(|p| p.id == id).unwrap();
-
+    if let Some(current) = processor.current_process_mut() {
         current.remaining_time -= 1;
         current.serviced_time += 1;
         if current.remaining_time > 0 {
             return;
         }
 
+        processor.set_current_process_state(ProcessState::Terminated);
         processor.current = None;
     }
+
     // At this point, the processor has no currently executed process
     for process in &processor.processes {
         if process.remaining_time > 0 {
             processor.current = Some(process.id);
+            break;
         }
     }
+
+    processor.set_current_process_state(ProcessState::Running);
 }
 
-fn shortes_job_first(processor: &mut Processor) {
-    if let Some(id) = processor.current {
-        let mut current = processor.processes.iter_mut().find(|p| p.id == id).unwrap();
-
+fn shortest_job_first(processor: &mut Processor) {
+    if let Some(current) = processor.current_process_mut() {
         current.remaining_time -= 1;
         current.serviced_time += 1;
         if current.remaining_time > 0 {
             return;
         }
 
+        processor.set_current_process_state(ProcessState::Terminated);
         processor.current = None;
     }
 
-    let (mut id, mut len) = (None, u64::MAX);
+    // At this point, the processor has no currently executed process
+    let (mut id, mut len) = (None, None);
     for process in processor.processes.iter().filter(|p| p.remaining_time != 0) {
-        if process.remaining_time < len {
-            (id, len) = (Some(process.id), process.remaining_time);
+        if process.remaining_time < len.unwrap_or(u64::MAX) {
+            (id, len) = (Some(process.id), Some(process.remaining_time));
         }
     }
+
     processor.current = id;
+    processor.set_current_process_state(ProcessState::Running);
 }
 
 fn main() {
@@ -198,14 +257,6 @@ fn main() {
 
     processes.sort();
 
-    let mut log: Vec<SchedulingEvent> = Vec::new();
-    let mut processor = Processor::new(processes, first_come_first_serve);
-
-    for _ in 0..30 {
-        let previous = processor.current;
-        processor.tick();
-        log.append(&mut SchedulingEvent::from(previous, &processor));
-    }
-
-    println!("{:#?}", log);
+    Processor::first_come_first_serve(processes.clone()).run();
+    Processor::shortest_job_first(processes.clone()).run();
 }
